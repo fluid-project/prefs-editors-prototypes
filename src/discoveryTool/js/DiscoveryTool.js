af57291e2f99ed9ee24fa692594985c57d65dcf0
Cindy Qi Li <cli@ocad.ca>
2013-08-01 15:48:24 -0400

    GPII-130: Code clean up.

--- src/discoveryTool/js/DiscoveryTool.js
+++ src/discoveryTool/js/DiscoveryTool.js
@@ -92,11 +92,10 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
     });
 
     gpii.discoveryTool.addFontIcon = function (toggleButton) {
-        // toggleButton.addClass("fl-icon-discover").attr({
-        //     "aria-label": "Discover",
-        //     "role": "presentation"
-        // });
-        toggleButton.html(toggleButton.html() + "<span class=\"fl-icon-discover\" aria-label=\"Discover\" role=\"presentation\"></span>");
+        toggleButton.addClass("fl-icon-discover").attr({
+            "aria-label": "Discover",
+            "role": "presentation"
+        });
     };
 
     gpii.discoveryTool.removeFontIcon = function (toggleButton) {
