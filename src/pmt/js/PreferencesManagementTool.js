3ec16b540dd44639d9e6341c6498defbdadfd82d
chrispetsos <chrispetsos@msn.com>
2013-08-06 13:18:37 +0300

    Added the text-size adjuster.

--- src/pmt/js/PreferencesManagementTool.js
+++ src/pmt/js/PreferencesManagementTool.js
@@ -6,7 +6,15 @@
 	        "minimum": 1,
 	        "maximum": 5,
 	        "divisibleBy": 0.2
-	    }/* ,
+	    },
+	    "fluid.uiOptions.textSize": {
+	        "type": "number",
+	        "default": 1,
+	        "minimum": 1,
+	        "maximum": 2,
+	        "divisibleBy": 0.1
+	    }
+	    /* ,
 	    "fluid.uiOptions.textFont": {
 	        "type": "string",
 	        "default": "",
@@ -17,6 +25,45 @@
 	        "default": false
 	    } */
 	};
+	
+	fluid.defaults("fluid.uiOptions.panels.textSize", {
+	    gradeNames: ["fluid.uiOptions.panels", "autoInit"],
+	    preferenceMap: {
+	        "fluid.uiOptions.textSize": {
+	            "model.value": "default",
+	            "range.min": "minimum",
+	            "range.max": "maximum"
+	        }
+	    },
+	    range: {
+	        min: 1,
+	        max: 2
+	    },
+	    selectors: {
+	        textSize: ".flc-uiOptions-text-size",
+	        label: ".flc-uiOptions-text-size-label",
+	        smallIcon: ".flc-uiOptions-text-size-smallIcon",
+	        largeIcon: ".flc-uiOptions-text-size-largeIcon",
+	        multiplier: ".flc-uiOptions-multiplier"
+	    },
+	    protoTree: {
+	        label: {messagekey: "textSizeLabel"},
+	        smallIcon: {messagekey: "textSizeSmallIcon"},
+	        largeIcon: {messagekey: "textSizelargeIcon"},
+	        multiplier: {messagekey: "multiplier"},
+	        textSize: {
+	            decorators: {
+	                type: "fluid",
+	                func: "fluid.uiOptions.textfieldSlider"
+	            }
+	        }
+	    },
+	    sliderOptions: {
+	        orientation: "horizontal",
+	        step: 0.1,
+	        range: "min"
+	    }
+	});
 	
 	fluid.defaults("fluid.uiOptions.panels.lineSpace", {
 	    gradeNames: ["fluid.uiOptions.panels", "autoInit"],
@@ -68,6 +115,27 @@
 	        "message": "%prefix/uiOptions.json",
 	 
 	        // The preference-specific information:
+	        "textSize": {
+	            "type": "fluid.uiOptions.textSize",
+	            "enactor": {
+	                "type": "fluid.uiOptions.enactors.textSize",
+	                "fontSizeMap": {
+	                    "xx-small": "9px",
+	                    "x-small": "11px",
+	                    "small": "13px",
+	                    "medium": "15px",
+	                    "large": "18px",
+	                    "x-large": "23px",
+	                    "xx-large": "30px"
+	                }
+	            },
+	            "panel": {
+	                "type": "fluid.uiOptions.panels.textSize",
+	                "container": ".flc-uiOptions-text-size",  // the css selector in the template where the panel is rendered
+	                "template": "%prefix/UIOptionsTemplate-textSize.html",
+	                "message": "%prefix/textSize.json"
+	            }
+	        } ,
 	        "lineSpace": {
 	            "type": "fluid.uiOptions.lineSpace",
 	            "enactor": {
@@ -88,7 +156,7 @@
 	                "template": "%prefix/UIOptionsTemplate-lineSpace.html",
 	                "message": "%prefix/lineSpace.json"
 	            }
-	        }/* ,
+	        }/*
 	        "textFont": {
 	            "type": "fluid.uiOptions.textFont",
 	            "classes": {
