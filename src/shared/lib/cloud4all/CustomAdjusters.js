d3946b5e19b0ab3f81122803dd6b82377b94a30d
chrispetsos <chrispetsos@msn.com>
2013-08-27 13:50:31 +0300

    Only allow numerics in plusMinusAdjusters.

--- src/shared/lib/cloud4all/CustomAdjusters.js
+++ src/shared/lib/cloud4all/CustomAdjusters.js
@@ -217,6 +217,20 @@
 						that.refreshView();
 					}
 			);
+			
+			// prevent non numeric values
+			that.locate("valueText").keydown(function(event) {
+                // Allow only backspace and delete
+                if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 35 || event.keyCode == 36) {
+                    // let it happen, don't do anything
+                }
+                else {
+                    // Ensure that it is a number and stop the keypress
+                    if ((event.keyCode < 48 || event.keyCode > 57 ) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                        event.preventDefault();
+                    }
+                }
+            });
 		});
 	};
     
