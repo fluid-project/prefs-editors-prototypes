1857b52795be52e97c04fbad5da152c8f7fa2993
Cindy Qi Li <cli@ocad.ca>
2013-09-06 09:46:07 -0400

    GPII-188: Updated panel components to use the new value reference method on the protoTree of renderer components.

--- src/shared/lib/cloud4all/CustomAdjusters.js
+++ src/shared/lib/cloud4all/CustomAdjusters.js
@@ -290,8 +290,8 @@
                 inputID: "magnifierPositionInput",
                 selectID: "magnifierPosition-radio",
                 tree: {
-                    optionnames: "{that}.options.strings.magnifierPosition",
-                    optionlist: "{that}.options.controlValues.magnifierPosition",
+                    optionnames: "${{that}.options.strings.magnifierPosition}",
+                    optionlist: "${{that}.options.controlValues.magnifierPosition}",
                     selection: "${value}"
                 }
             }
@@ -372,8 +372,8 @@
                 inputID: "magnifierFollowsInput",
                 selectID: "magnifierFollows-radio",
                 tree: {
-                    optionnames: "{that}.options.strings.magnifierFollows",
-                    optionlist: "{that}.options.controlValues.magnifierFollows",
+                    optionnames: "${{that}.options.strings.magnifierFollows}",
+                    optionlist: "${{that}.options.controlValues.magnifierFollows}",
                     selection: "${value}"
                 }
             }
