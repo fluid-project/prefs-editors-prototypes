77247ff40d42f2f12864f947b8b772a5b5b50512
Justin Obara <obara.justin@gmail.com>
2013-09-27 14:28:52 -0400

    PMT refactoring and messages bundles
    Refactored PMT to be more declarative and to make use of the message bundles. There are still factoring issues with the fluid.uiOptions.pmt component, but hopefully these will be addressed when complex panels are better supported in the preferences framework (FLUID-5131). Naming should also be re-evaluated after FLUID-5131.

--- demos/pmt/demo.js
+++ demos/pmt/demo.js
@@ -1,25 +1,15 @@
 var demo = demo || {};
 (function ($, fluid) {
 
-    demo.initWithSchema = function (container, compOpts, uioType) {
-    	var uioBuilder = fluid.uiOptions.builder({
+    demo.initPMT = function (container, compOpts) {
+        var uioBuilder = fluid.uiOptions.builder({
             primarySchema: fluid.uiOptions.pmt.primarySchema,
             auxiliarySchema: fluid.uiOptions.pmt.auxiliarySchema
         });
         var baseOpts = {
-            uioType: uioType
+            uioType: "fluid.uiOptions.pmt"
         };
         $.extend(true, baseOpts, compOpts);
         return fluid.invokeGlobalFunction(uioBuilder.options.assembledUIOGrade, [container, baseOpts]);
     };
-
-    demo.initFullWithPreview = function (container, options) {
-        return demo.initWithSchema(container, options, "fluid.uiOptions.fullPreview");
-    };
-
-    demo.initFullNoPreview = function (container, options) {
-        return demo.initWithSchema(container, options, "fluid.uiOptions.fullNoPreview");
-    };
-
-    demo.initFinishedEvent = setupPMT;
 })(jQuery, fluid);
