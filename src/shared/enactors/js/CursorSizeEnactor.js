7a990e69fd5d7cf449698736c03ac44f59d080ad
chrispetsos <chrispetsos@msn.com>
2013-10-15 11:53:15 +0300

    Cursor size enactor has been made more compact and declarative.

--- src/shared/enactors/js/CursorSizeEnactor.js
+++ src/shared/enactors/js/CursorSizeEnactor.js
@@ -7,30 +7,25 @@
                 "model.value": "default"
             }
         },
+        selectors: {
+        	cursorDiv: ".flc-uiOptions-preview-per-setting-cursor-div"
+        },
         invokers: {
             set: {
                 funcName: "gpii.uiOptions.enactors.cursorSize.set",
-                args: ["{arguments}.0", "{that}"]
+                args: ["{that}.model.value", "{that}"]
             }
         },
         listeners: {
-            onCreate: {
-                listener: "{that}.set",
-                args: "{that}.model.value"
-            }
+            "onCreate.init": {
+                listener: "{that}.applier.modelChanged.addListener",
+                args: ["value", "{that}.set"]
+            },
         }
     });
 
     gpii.uiOptions.enactors.cursorSize.set = function (times, that) {
-        that.container.find(".flc-uiOptions-preview-per-setting-cursor1").css("font-size", times + "em");
-        that.container.find(".flc-uiOptions-preview-per-setting-cursor2").css("font-size", times + "em");
-        that.container.find(".flc-uiOptions-preview-per-setting-cursor3").css("font-size", times + "em");
-    };
-
-    gpii.uiOptions.enactors.cursorSize.finalInit = function (that) {
-        that.applier.modelChanged.addListener("value", function (newModel) {
-            that.set(newModel.value);
-        });
+    	that.locate("cursorDiv").css("font-size", times + "em");
     };
 
 })(jQuery, fluid);
