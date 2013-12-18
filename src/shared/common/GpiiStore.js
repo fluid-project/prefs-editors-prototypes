439de392ac219b80ca6907adcf36f0307ffa6789
chrispetsos <chrispetsos@msn.com>
2013-12-18 14:48:13 +0200

    GPII-265: First working version with GPII store. Using local server. Had to patch file /home/bob/git/node_modules/universal/node_modules/kettle/lib/server.js to allow cross-domain requests with this change,
        kettle.server.config.configureCreateRequest = function (server, requests) {
            server.configure(function () {
                server.use(requests.create);
    		server.use(function (req, res, next) {
    		 res.header("Access-Control-Allow-Origin", "*");
    		 res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
    		 res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
    		 next();
    		});
            });
        };

---
+++ src/shared/common/GpiiStore.js
@@ -0,0 +1,105 @@
+/*
+Copyright 2013 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global demo:true, fluid, gpii, jQuery, window*/
+
+// JSLint options
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+// GPII store is for connecting the discovery tool with the GPII server.
+// The discovery tool uses the cookie store by default, rather than the
+// GPII store.
+// To activate the GPII store in the discovery tool, refer to comments in
+// http://issues.gpii.net/browse/GPII-185
+
+(function ($, fluid) {
+
+    fluid.registerNamespace("gpii.prefs");
+
+    /**
+     * gpiiStore Subcomponent that uses GPII server for persistence.
+     * It sends request to the GPII server to save and retrieve model information
+     * @param {Object} options
+     */
+    fluid.defaults("gpii.prefs.gpiiStore", {
+        gradeNames: ["fluid.prefs.dataSource", "autoInit"],
+        gpiiEntry: "http://registry.gpii.org/applications/gpii.prefs",
+        invokers: {
+            get: {
+                funcName: "gpii.prefs.gpiiStore.get",
+                args: ["{that}.options"]
+            },
+            set: {
+                funcName: "gpii.prefs.gpiiStore.set",
+                args: ["{arguments}.0", "{that}.options"]
+            }
+        }
+    });
+
+    gpii.prefs.gpiiStore.get = function (settings) {
+        var gpiiModel;
+
+        $.ajax({
+            url: settings.url,
+            type: "GET",
+            dataType: "json",
+            async: false,
+            success: function (data) {
+                gpiiModel = fluid.get(data, [settings.gpiiEntry, 1, "value"]);
+                var i=0;
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                fluid.log("GET: Error at retrieving from GPII! Test status: " + textStatus);
+                fluid.log(errorThrown);
+            }
+        });
+
+        return gpiiModel;
+    };
+
+    gpii.prefs.gpiiStore.set = function (model, settings) {
+        var dataToSave = {};
+
+        dataToSave[settings.gpiiEntry] = [{
+            value: model
+        }];
+
+        $.ajax({
+            url: settings.url,
+            type: "POST",
+            //contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(dataToSave),
+            success: function (data) {
+                fluid.log("POST: Saved to GPII server");
+            },
+            error: function () {
+                fluid.log("POST: Error at saving to GPII server");
+            }
+        });
+    };
+
+    fluid.defaults("gpii.prefs.gpiiSettingsStore", {
+        gradeNames: ["autoInit", "fluid.globalSettingsStore"],
+        storeType: "gpii.prefs.gpiiStore",
+        distributeOptions: [{
+            source: "{that}.options.storeType",
+            removeSource: true,
+            target: "{that > settingsStore}.type"
+        }, {
+            source: "{that}.options.settingsStore",
+            removeSource: true,
+            target: "{that > settingsStore}.options"
+        }]
+    });
+
+})(jQuery, fluid);
