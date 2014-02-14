b1939eeb874f207b0d065a90d96004b03389dbf2
Alexander Milchev <amilchev@asteasolutions.com>
2014-02-14 15:35:52 +0200

    GPII-565: Deleted adjusters with unsupported live updates from PCP aux schema.

--- src/shared/schemas/js/pcpSchema.js
+++ src/shared/schemas/js/pcpSchema.js
@@ -45,53 +45,10 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
                         "always": ["contrastEnabled"],
                         "gpii.primarySchema.contrastEnabled": ["contrastTheme"]
                     }
-                },
-                "increaseSize": {
-                    "type": "gpii.panel.increaseSizePCP",
-                    "container": ".gpiic-prefsEditor-increaseSizePanel",
-                    "template": "%prefix/increaseSizeTemplatePCP.html",
-                    "message": "%prefix/increaseSize.json",
-                    "panels": {
-                        "always": ["textSize", "cursorSize", "magnifierEnabled"],
-                        "gpii.primarySchema.magnifierEnabled": ["magnifier"]
-                    }
-                },
-                "visualAlternatives": {
-                    "type": "gpii.panel.visualAlternatives",
-                    "container": ".gpiic-visualAlternatives",
-                    "template": "../../src/pcp/html/pcp-VisualAlternativesGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["speakText", "screenReaderBrailleOutput"],
-                        "gpii.primarySchema.speakText": [
-                            "wordsSpokenPerMinute", "volume"
-                        ]
-                    }
-                },
-                "volume": {
-                    "type": "gpii.panel.volumeCollectivePanel",
-                    "container": ".gpiic-volumeGroup",
-                    "template": "%prefix/VolumeGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["universalVolume"]
-                    }
-                },
-                "language": {
-                    "type": "gpii.panel.languageCollectivePanel",
-                    "container": ".gpiic-languageGroup",
-                    "template": "%prefix/LanguageGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["universalLanguage"]
-                    }
                 }
             },
             "contrastEnabled": {
                 "type": "gpii.primarySchema.contrastEnabled",
-                /*"enactor": {
-                    "type": "gpii.enactor.contrastTheme"
-                },*/
                 "panel": {
                     "type": "gpii.adjuster.contrastEnabled",
                     "container": ".gpiic-contrastEnabled",
@@ -107,123 +64,12 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
                     "black-yellow": "fl-theme-prefsEditor-by gpii-prefsEditor-theme-by fl-theme-by",
                     "yellow-black": "fl-theme-prefsEditor-yb gpii-prefsEditor-theme-yb fl-theme-yb"
                 },
-                // "enactor": {
-                //     "type": "gpii.enactor.contrastThemeNoPreview"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.contrastThemeNoPreview",
                     "container": ".gpiic-contrastTheme",
                     "template": "%prefix/contrastThemeNoPreviewTemplate.html",
                     "message": "%prefix/contrast.json",
                     "classnameMap": {"theme": "@contrastTheme.classes"}
-                }
-            },
-            "textSize": {
-                "type": "gpii.primarySchema.fontSize",
-                // "enactor": {
-                //     "type": "gpii.enactor.textSize"
-                // },
-                "panel": {
-                    "type": "gpii.adjuster.textSizePCP",
-                    "container": ".gpiic-prefsEditor-textSize",
-                    "template": "%prefix/textSizeTemplatePCP.html",
-                    "message": "%prefix/textSize.json"
-                }
-            },
-            "cursorSize": {
-                "type": "gpii.primarySchema.cursorSize",
-                // "enactor": {
-                //     "type": "gpii.enactor.cursorSize"
-                // },
-                "panel": {
-                    "type": "gpii.adjuster.cursorSizePCP",
-                    "container": ".gpiic-prefsEditor-cursorSize",
-                    "template": "%prefix/cursorSizeTemplatePCP.html",
-                    "message": "%prefix/cursorSize.json"
-                }
-            },
-            "magnifierEnabled": {
-                "type": "gpii.primarySchema.magnifierEnabled",
-                // "enactor": {
-                //     "type": "gpii.enactor.magnifier"
-                // },
-                "panel": {
-                    "type": "gpii.adjuster.magnifierEnabled",
-                    "container": ".gpiic-prefsEditor-magnifierEnabled",
-                    "template": "%prefix/onOffSwitchTemplate.html",
-                    "message": "%prefix/magnifier.json"
-                }
-            },
-            "magnifier": {
-                "type": "gpii.primarySchema.magnification",
-                // "enactor": {
-                //     "type": "gpii.enactor.magnifier"
-                // },
-                "panel": {
-                    "type": "gpii.adjuster.magnifierPCP",
-                    "container": ".gpiic-prefsEditor-magnifier",
-                    "template": "%prefix/magnifierTemplatePCP.html",
-                    "message": "%prefix/magnifier.json"
-                }
-            },
-
-            "speakText": {
-                "type": "gpii.primarySchema.speakText",
-                "panel": {
-                    "type": "gpii.adjuster.speakText",
-                    "template": "%prefix/speakTextTemplate.html",
-                    "container": ".speakText",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "wordsSpokenPerMinute": {
-                "type": "gpii.primarySchema.wordsSpokenPerMinute",
-                "panel": {
-                    "type": "gpii.adjuster.wordsSpokenPerMinute",
-                    "template": "%prefix/wordsSpokenPerMinute.html",
-                    "container": ".words-spoken-per-minute",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "volume": {
-                "type": "gpii.primarySchema.volume",
-                "panel": {
-                    "type": "gpii.adjuster.volume",
-                    "template": "%prefix/volumeTemplate.html",
-                    "container": ".volume",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "screenReaderBrailleOutput": {
-                "type": "gpii.primarySchema.screenReaderBrailleOutput",
-                "panel": {
-                    "type": "gpii.adjuster.screenReaderBrailleOutput",
-                    "template": "%prefix/brailleTemplate.html",
-                    "container": ".braille",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "universalVolume": {
-                "type": "gpii.primarySchema.universalVolume",
-                "panel": {
-                    "type": "gpii.adjuster.universalVolume",
-                    "template": "%prefix/universalVolumeTemplate.html",
-                    "container": ".universalVolume",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "universalLanguage": {
-                "type": "gpii.primarySchema.universalLanguage",
-                "panel": {
-                    "type": "gpii.adjuster.universalLanguage",
-                    "template": "%prefix/universalLanguageTemplate.html",
-                    "container": ".universalLanguage",
-                    "message": "%prefix/message.json"
                 }
             }
         }
@@ -252,43 +98,10 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
                         "always": ["textSize", "cursorSize", "magnifierEnabled"],
                         "gpii.primarySchema.magnifierEnabled": ["magnifier"]
                     }
-                },
-                "visualAlternatives": {
-                    "type": "gpii.panel.visualAlternatives",
-                    "container": ".gpiic-visualAlternatives",
-                    "template": "../../src/pcp/html/pcp-VisualAlternativesGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["speakText", "screenReaderBrailleOutput"],
-                        "gpii.primarySchema.speakText": [
-                            "wordsSpokenPerMinute", "volume"
-                        ]
-                    }
-                },
-                "volume": {
-                    "type": "gpii.panel.volumeCollectivePanel",
-                    "container": ".gpiic-volumeGroup",
-                    "template": "%prefix/VolumeGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["universalVolume"]
-                    }
-                },
-                "language": {
-                    "type": "gpii.panel.languageCollectivePanel",
-                    "container": ".gpiic-languageGroup",
-                    "template": "%prefix/LanguageGroupContainer.html",
-                    "message": "%prefix/message.json",
-                    "panels": {
-                        "always": ["universalLanguage"]
-                    }
                 }
             },
             "contrastEnabled": {
                 "type": "gpii.primarySchema.contrastEnabled",
-                /*"enactor": {
-                    "type": "gpii.enactor.contrastTheme"
-                },*/
                 "panel": {
                     "type": "gpii.adjuster.contrastEnabled",
                     "container": ".gpiic-contrastEnabled",
@@ -304,9 +117,6 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
                     "black-yellow": "fl-theme-prefsEditor-by gpii-prefsEditor-theme-by fl-theme-by",
                     "yellow-black": "fl-theme-prefsEditor-yb gpii-prefsEditor-theme-yb fl-theme-yb"
                 },
-                // "enactor": {
-                //     "type": "gpii.enactor.contrastThemeNoPreview"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.contrastThemeNoPreview",
                     "container": ".gpiic-contrastTheme",
@@ -317,9 +127,6 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
             },
             "textSize": {
                 "type": "gpii.primarySchema.fontSize",
-                // "enactor": {
-                //     "type": "gpii.enactor.textSize"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.textSizePCP",
                     "container": ".gpiic-prefsEditor-textSize",
@@ -329,9 +136,6 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
             },
             "cursorSize": {
                 "type": "gpii.primarySchema.cursorSize",
-                // "enactor": {
-                //     "type": "gpii.enactor.cursorSize"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.cursorSizePCP",
                     "container": ".gpiic-prefsEditor-cursorSize",
@@ -341,9 +145,6 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
             },
             "magnifierEnabled": {
                 "type": "gpii.primarySchema.magnifierEnabled",
-                // "enactor": {
-                //     "type": "gpii.enactor.magnifier"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.magnifierEnabled",
                     "container": ".gpiic-prefsEditor-magnifierEnabled",
@@ -353,74 +154,11 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
             },
             "magnifier": {
                 "type": "gpii.primarySchema.magnification",
-                // "enactor": {
-                //     "type": "gpii.enactor.magnifier"
-                // },
                 "panel": {
                     "type": "gpii.adjuster.magnifierPCP",
                     "container": ".gpiic-prefsEditor-magnifier",
                     "template": "%prefix/magnifierTemplatePCP.html",
                     "message": "%prefix/magnifier.json"
-                }
-            },
-
-            "speakText": {
-                "type": "gpii.primarySchema.speakText",
-                "panel": {
-                    "type": "gpii.adjuster.speakText",
-                    "template": "%prefix/speakTextTemplate.html",
-                    "container": ".speakText",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "wordsSpokenPerMinute": {
-                "type": "gpii.primarySchema.wordsSpokenPerMinute",
-                "panel": {
-                    "type": "gpii.adjuster.wordsSpokenPerMinute",
-                    "template": "%prefix/wordsSpokenPerMinute.html",
-                    "container": ".words-spoken-per-minute",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "volume": {
-                "type": "gpii.primarySchema.volume",
-                "panel": {
-                    "type": "gpii.adjuster.volume",
-                    "template": "%prefix/volumeTemplate.html",
-                    "container": ".volume",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "screenReaderBrailleOutput": {
-                "type": "gpii.primarySchema.screenReaderBrailleOutput",
-                "panel": {
-                    "type": "gpii.adjuster.screenReaderBrailleOutput",
-                    "template": "%prefix/brailleTemplate.html",
-                    "container": ".braille",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "universalVolume": {
-                "type": "gpii.primarySchema.universalVolume",
-                "panel": {
-                    "type": "gpii.adjuster.universalVolume",
-                    "template": "%prefix/universalVolumeTemplate.html",
-                    "container": ".universalVolume",
-                    "message": "%prefix/message.json"
-                }
-            },
-
-            "universalLanguage": {
-                "type": "gpii.primarySchema.universalLanguage",
-                "panel": {
-                    "type": "gpii.adjuster.universalLanguage",
-                    "template": "%prefix/universalLanguageTemplate.html",
-                    "container": ".universalLanguage",
-                    "message": "%prefix/message.json"
                 }
             }
         }
