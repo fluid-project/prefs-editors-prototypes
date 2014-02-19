21a6e6c5e0b54f10532277b3f03fec42108f22ec
chrispetsos <chrispetsos@msn.com>
2014-02-19 13:42:02 +0200

    GPII-593: OnOffSwitch adjusters can be toggled using the Enter key.

--- src/shared/adjusters/js/onOffSwitch.js
+++ src/shared/adjusters/js/onOffSwitch.js
@@ -19,7 +19,39 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
         gradeNames: ["fluid.prefs.panel", "autoInit"],
         selectors: {
             headingLabel: ".gpiic-onOffSwitch-label",
-            valueCheckbox: ".gpiic-onOffSwitch-input"
+            valueCheckbox: ".gpiic-onOffSwitch-input",
+            onOffSwitch: ".gpiic-prefsEditor-onOffSwitch"
+        },
+        selectorsToIgnore: ["onOffSwitch"],
+        listeners: {
+            "onDomBind.bindModelChangeOnKeyPressEvent": {
+                "this": "{that}.dom.onOffSwitch",
+                "method": "keypress",
+                "args": ["{that}.triggerModelChangeOnEnterKey"]
+            }
+        },
+        invokers: {
+            triggerModelChangeOnEnterKey: {
+                "funcName": "gpii.adjuster.onOffSwitch.triggerModelChangeOnEnterKey",
+                "args": [
+                    "{arguments}.0", // keypress event
+                    "{that}"
+                ]
+            }
         }
     });
+    
+    gpii.adjuster.onOffSwitch.triggerModelChangeOnEnterKey = function (keypressEvent, that) {
+        if (keypressEvent.which == 13) {
+            /*
+             * Isn't this the most appropriate way of performing the model change compared to requestChange?
+             * The reason for this is that the actual model path will be known only to the sub-components
+             * of this one (e.g. the magnifierEnabled). So, we either do it this way or we force/anticipate
+             * that sub-component implementers will follow the forced "that.model.value" path...
+             */
+            that.locate("valueCheckbox").click();
+            //that.applier.requestChange("value", !that.model.value)
+        }
+    };
+    
 })(jQuery, fluid);
