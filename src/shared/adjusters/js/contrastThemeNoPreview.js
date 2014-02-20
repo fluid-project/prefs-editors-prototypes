643699b1b570bb00ed1b68b6cef9ad8dfcdd4890
chrispetsos <chrispetsos@msn.com>
2014-02-20 14:53:20 +0200

    GPII-590: Made "contrastTheme" adjuster's selections activatable labels.

--- src/shared/adjusters/js/contrastThemeNoPreview.js
+++ src/shared/adjusters/js/contrastThemeNoPreview.js
@@ -16,7 +16,7 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
 (function ($, fluid) {
     
     fluid.defaults("gpii.adjuster.contrastThemeNoPreview", {
-        gradeNames: ["fluid.prefs.panel.contrast", "autoInit"],
+        gradeNames: ["fluid.prefs.panel.contrast", "gpii.adjuster.activatableLabels", "autoInit"],
         mergePolicy: {
             "controlValues.theme": gpii.prefs.merging.arrayOverridePolicy,
             "stringArrayIndex.theme": gpii.prefs.merging.arrayOverridePolicy
@@ -29,6 +29,7 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
         selectors: {
             colourTheme: ".gpiic-contrast-colourTheme"
         },
+        activatableLabelsSelector: "{that}.options.selectors.themeLabel",
         listeners: {
             "onDomBind.setColourThemeText": {
                 "this": "{that}.dom.colourTheme",
