eda497c59d59e7d181d167c00ca0a002069caafd
chrispetsos <chrispetsos@msn.com>
2014-02-27 10:30:22 +0200

    GPII-590: Corrected copyright dates in new files.

--- src/shared/adjusters/js/activatableLabelsClickOnActivate.js
+++ src/shared/adjusters/js/activatableLabelsClickOnActivate.js
@@ -1,7 +1,7 @@
 /*!
 Cloud4all Preferences Management Tools
 
-Copyright 2013 CERTH/HIT
+Copyright 2014 CERTH/HIT
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
