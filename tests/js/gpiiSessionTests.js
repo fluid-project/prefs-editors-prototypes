18636224bccbfc0975ef6e7e5303e608c522930e
chrispetsos <chrispetsos@msn.com>
2014-03-12 11:28:07 +0200

    GPII-657: Refactored the code to use a single, test-generating function.

--- tests/js/gpiiSessionTests.js
+++ tests/js/gpiiSessionTests.js
@@ -15,12 +15,20 @@ https://github.com/gpii/universal/LICENSE.txt
 /*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
 
 (function ($) {
+    fluid.registerNamespace("gpii.prefs.gpiiSession.tests");
+
     jqUnit.module("GPIISession Tests");
 
+    // The session object to work with throughout the tests
     var session = gpii.prefs.gpiiSession();
+    
+    // Test data
     var userToWorkWith = "alsa";
     var non_existent_user = "non_existent_user";
     
+    /*
+     * Mock settings used in the various test scenarios.
+     */
     var loginSuccessMockSettings = 
     {
         url: session.options.url + "user/" + userToWorkWith + "/login",
@@ -63,74 +71,82 @@ https://github.com/gpii/universal/LICENSE.txt
         responseText: '{"isError": true, "message": "There was an error logging out user ' + userToWorkWith + '"}'
     };
     
-    jqUnit.test("no user initially logged in", function () {
-        jqUnit.assertNoValue("Initially, no user should be logged in", session.options.loggedUser);
-    });
+    /*
+     * State-changing and assertion functions used in the various tests.
+     */
+    gpii.prefs.gpiiSession.tests.assertNoUserInitially = function () {
+        jqUnit.assertNoValue("Initially, no user should be logged in", session.options.loggedUser);        
+    };
     
-    jqUnit.test("login user", function () {
-        $.mockjaxClear();
-        $.mockjax(loginSuccessMockSettings);
-
+    gpii.prefs.gpiiSession.tests.assertLoginUser = function () {
         session.login(userToWorkWith);
         jqUnit.assertEquals("User '" + userToWorkWith + "' should now be logged in", userToWorkWith, session.options.loggedUser);
-        $.mockjaxClear();
-    });
+    };
     
-    jqUnit.test("get logged user", function () {
-        $.mockjaxClear();
-        $.mockjax(tokenSuccessMockSettings);
-        
+    gpii.prefs.gpiiSession.tests.assertGetLoggedUser = function () {
         session.getLoggedUser();
         jqUnit.assertEquals("Current user should be '" + userToWorkWith + "'", userToWorkWith, session.options.loggedUser);
-        $.mockjaxClear();
-    });
-
-    jqUnit.test("logout user", function () {
-        $.mockjaxClear();
-        $.mockjax(logoutSuccessMockSettings);
-
+    };
+    
+    gpii.prefs.gpiiSession.tests.assertLogoutUser = function () {
         session.logout();
         jqUnit.assertNoValue("No user should be currently logged in", session.options.loggedUser);
-        $.mockjaxClear();
-    });
-
-    jqUnit.test("login user error", function () {
-        $.mockjaxClear();
-        $.mockjax(loginErrorMockSettings);
-
+    };
+    
+    gpii.prefs.gpiiSession.tests.assertLoginUserError = function () {
         session.login(non_existent_user);
         jqUnit.assertNoValue("Should not be able to login 'non_existent_user'", session.options.loggedUser);
-        $.mockjaxClear();
-    });
+    };
     
-    jqUnit.test("get logged user error", function () {
-        $.mockjaxClear();
-        $.mockjax(tokenErrorMockSettings);
-        
+    gpii.prefs.gpiiSession.tests.assertGetLoggedUserError = function () {
         session.getLoggedUser();
         jqUnit.assertNoValue("No user should be currently logged into the system", session.options.loggedUser);
-        $.mockjaxClear();
-    });
-
-    jqUnit.test("login " + userToWorkWith + " and logout user error", function () {
-        $.mockjaxClear();
-        $.mockjax(loginSuccessMockSettings);
-        $.mockjax(logoutErrorMockSettings);
-
+    };
+    
+    gpii.prefs.gpiiSession.tests.assertLoginUserLogoutError = function () {
         session.login(userToWorkWith);
         session.logout();
         jqUnit.assertEquals("User '" + userToWorkWith + "' should still be logged in", userToWorkWith, session.options.loggedUser);
-        $.mockjaxClear();
-    });
-
-    jqUnit.test("set logged user", function () {
+    };
+    
+    gpii.prefs.gpiiSession.tests.assertSetLoggedUser = function () {
         session.setLoggedUser(userToWorkWith);
         jqUnit.assertEquals("User '" + userToWorkWith + "' should have been logged in externally", userToWorkWith, session.options.loggedUser);
-    });
-
-    jqUnit.test("clear logged user", function () {
+    };
+    
+    gpii.prefs.gpiiSession.tests.assertClearLoggedUser = function () {
         session.clearLoggedUser();
         jqUnit.assertNoValue("User should have been logged out externally", session.options.loggedUser);
-    });
-
+    };
+    
+    /*
+     * Reusable helper function that generates a test given:
+     * 1) The test title
+     * 2) The assertion function to be used in the test
+     * 3) An array of enabled mock settings (if any) 
+     */
+    gpii.prefs.gpiiSession.tests.mockTest = function (testTitle, assertFunction, enabledMockSettings) {
+        jqUnit.test(testTitle, function () {
+            $.mockjaxClear();
+            if(enabledMockSettings) {
+                $.each(enabledMockSettings, function(index, mockSetting) {
+                    $.mockjax(mockSetting);
+                });
+            }
+            assertFunction();
+            $.mockjaxClear();
+        });
+    };
+    
+    // Perform the various tests.
+    gpii.prefs.gpiiSession.tests.mockTest("no user initially logged in", gpii.prefs.gpiiSession.tests.assertNoUserInitially);
+    gpii.prefs.gpiiSession.tests.mockTest("login user", gpii.prefs.gpiiSession.tests.assertLoginUser, [loginSuccessMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("get logged user", gpii.prefs.gpiiSession.tests.assertGetLoggedUser, [tokenSuccessMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("logout user", gpii.prefs.gpiiSession.tests.assertLogoutUser, [logoutSuccessMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("login user error", gpii.prefs.gpiiSession.tests.assertLoginUserError, [loginErrorMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("get logged user error", gpii.prefs.gpiiSession.tests.assertGetLoggedUserError, [tokenErrorMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("login " + userToWorkWith + " and logout user error", gpii.prefs.gpiiSession.tests.assertLoginUserLogoutError, [loginSuccessMockSettings, logoutErrorMockSettings]);
+    gpii.prefs.gpiiSession.tests.mockTest("set logged user", gpii.prefs.gpiiSession.tests.assertSetLoggedUser);
+    gpii.prefs.gpiiSession.tests.mockTest("clear logged user", gpii.prefs.gpiiSession.tests.assertClearLoggedUser);
+    
 })(jQuery);
