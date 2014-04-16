0297d7e9e19cde794996e6bae09cd3c22564c140
Alexander Milchev <amilchev@asteasolutions.com>
2014-04-16 11:36:08 +0300

    GPII-663: Converted example model is now object, rather than string.

--- tests/js/gpiiStoreTests.js
+++ tests/js/gpiiStoreTests.js
@@ -24,40 +24,27 @@ https://github.com/gpii/universal/LICENSE.txt
     var userToWorkWith = "sammy";
     var userToBeCreated = "userThatJustHasBeenCreated";
 
+    var exampleModel = {"gpii_primarySchema_volume": 80, "gpii_primarySchema_keyEcho": false, "gpii_primarySchema_wordEcho": false,
+                        "gpii_primarySchema_fontSize": 12, "gpii_primarySchema_speakText": false, "gpii_primarySchema_voicePitch": 80,
+                        "gpii_primarySchema_cursorSize": 1, "gpii_primarySchema_magnification": 100, "gpii_primarySchema_contrast_theme": "black-white",
+                        "gpii_primarySchema_showCrosshairs": false, "gpii_primarySchema_universalVolume": 80, "gpii_primarySchema_contrastEnabled": false,
+                        "gpii_primarySchema_announceCapitals": false, "gpii_primarySchema_textHighlighting": "Word",
+                        "gpii_primarySchema_magnifierEnabled": false, "gpii_primarySchema_universalLanguage": "en",
+                        "gpii_primarySchema_punctuationVerbosity": "none", "gpii_primarySchema_screenReaderLanguage": "en",
+                        "gpii_primarySchema_wordsSpokenPerMinute": 130, "gpii_primarySchema_speakTutorialMessages": false,
+                        "gpii_primarySchema_magnificationPosition": "TopHalf", "gpii_primarySchema_screenReaderBrailleOutput": false};
 
-     convertedExampleModel = '{"http://registry.gpii.org/common/pitch":[{"value":0.8}],' +
-                                 '"http://registry.gpii.org/common/volume":[{"value":0.8}],' +
-                                 '"http://registry.gpii.org/common/keyEcho":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/language":[{"value":"en"}],' +
-                                 '"http://registry.gpii.org/common/wordEcho":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/fontSize":[{"value":12}],' +
-                                 '"http://registry.gpii.org/common/volumeTTS":[{"value":0.8}],' +
-                                 '"http://registry.gpii.org/common/speechRate":[{"value":130}],' +
-                                 '"http://registry.gpii.org/common/cursorSize":[{"value":0.2}],' +
-                                 '"http://registry.gpii.org/common/readingUnit":[{"value":"Word"}],' +
-                                 '"http://registry.gpii.org/common/magnification":[{"value":1}],' +
-                                 '"http://registry.gpii.org/common/showCrosshairs":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/announceCapitals":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/magnifierEnabled":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/highContrastTheme":[{"value":"black-white"}],' +
-                                 '"http://registry.gpii.org/common/magnifierPosition":[{"value":"TopHalf"}],' +
-                                 '"http://registry.gpii.org/common/auditoryOutLanguage":[{"value":"en"}],' +
-                                 '"http://registry.gpii.org/common/highContrastEnabled":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/punctuationVerbosity":[{"value":"none"}],' +
-                                 '"http://registry.gpii.org/common/speakTutorialMessages":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/screenReaderTTSEnabled":[{"value":false}],' +
-                                 '"http://registry.gpii.org/common/screenReaderBrailleOutput":[{"value":false}]}';
-
-
- var exampleModel = {"gpii_primarySchema_volume": 80, "gpii_primarySchema_keyEcho": false, "gpii_primarySchema_wordEcho": false,
-                     "gpii_primarySchema_fontSize": 12, "gpii_primarySchema_speakText": false, "gpii_primarySchema_voicePitch": 80,
-                     "gpii_primarySchema_cursorSize": 1, "gpii_primarySchema_magnification": 100, "gpii_primarySchema_contrast_theme": "black-white",
-                     "gpii_primarySchema_showCrosshairs": false, "gpii_primarySchema_universalVolume": 80, "gpii_primarySchema_contrastEnabled": false,
-                     "gpii_primarySchema_announceCapitals": false, "gpii_primarySchema_textHighlighting": "Word",
-                     "gpii_primarySchema_magnifierEnabled": false, "gpii_primarySchema_universalLanguage": "en",
-                     "gpii_primarySchema_punctuationVerbosity": "none", "gpii_primarySchema_screenReaderLanguage": "en",
-                     "gpii_primarySchema_wordsSpokenPerMinute": 130, "gpii_primarySchema_speakTutorialMessages": false,
-                     "gpii_primarySchema_magnificationPosition": "TopHalf", "gpii_primarySchema_screenReaderBrailleOutput": false};
+    var convertedExampleModel = {"http://registry.gpii.org/common/pitch":[{"value":0.8}], "http://registry.gpii.org/common/volume":[{"value":0.8}],
+                                 "http://registry.gpii.org/common/keyEcho":[{"value":false}], "http://registry.gpii.org/common/language":[{"value":"en"}],
+                                 "http://registry.gpii.org/common/wordEcho":[{"value":false}], "http://registry.gpii.org/common/fontSize":[{"value":12}],
+                                 "http://registry.gpii.org/common/volumeTTS":[{"value":0.8}], "http://registry.gpii.org/common/speechRate":[{"value":130}],
+                                 "http://registry.gpii.org/common/cursorSize":[{"value":0.2}], "http://registry.gpii.org/common/readingUnit":[{"value":"Word"}],
+                                 "http://registry.gpii.org/common/magnification":[{"value":1}], "http://registry.gpii.org/common/showCrosshairs":[{"value":false}],
+                                 "http://registry.gpii.org/common/announceCapitals":[{"value":false}], "http://registry.gpii.org/common/magnifierEnabled":[{"value":false}],
+                                 "http://registry.gpii.org/common/highContrastTheme":[{"value":"black-white"}], "http://registry.gpii.org/common/magnifierPosition":[{"value":"TopHalf"}],
+                                 "http://registry.gpii.org/common/auditoryOutLanguage":[{"value":"en"}], "http://registry.gpii.org/common/highContrastEnabled":[{"value":false}],
+                                 "http://registry.gpii.org/common/punctuationVerbosity":[{"value":"none"}], "http://registry.gpii.org/common/speakTutorialMessages":[{"value":false}],
+                                 "http://registry.gpii.org/common/screenReaderTTSEnabled":[{"value":false}], "http://registry.gpii.org/common/screenReaderBrailleOutput":[{"value":false}]}; 
 
 
     var loginSuccessMockSettings =
@@ -105,12 +92,11 @@ https://github.com/gpii/universal/LICENSE.txt
         var transformedExampleModel = store.modelTransform(exampleModel, gpii.prefs.commonTermsTransformationRules);
         var stringifiedExampleObject = JSON.stringify(transformedExampleModel); // Comparing stringified objects, since JSON.parse(JSON.stringify(obj)) != obj
 
-        jqUnit.assertEquals("Transforming the model from schema to http://registry.gpii.org/common/...", stringifiedExampleObject, convertedExampleModel);
+        jqUnit.assertEquals("Transforming the model from schema to http://registry.gpii.org/common/...", stringifiedExampleObject, JSON.stringify(convertedExampleModel));
     };
 
     gpii.prefs.gpiiStore.tests.assertInvertedModeflTransofrmation = function () {
-        var exampleTransformedObject = JSON.parse(convertedExampleModel);
-        var inverseTransformedExampleModel = store.inverseModelTransform(exampleTransformedObject, gpii.prefs.commonTermsInverseTransformationRules);
+        var inverseTransformedExampleModel = store.inverseModelTransform(convertedExampleModel, gpii.prefs.commonTermsInverseTransformationRules);
         var stringifiedExampleObject = JSON.stringify(inverseTransformedExampleModel);
 
         jqUnit.assertEquals("Transforming the model from http://registry.gpii.org/common/... to schema-like", stringifiedExampleObject, JSON.stringify(exampleModel));
