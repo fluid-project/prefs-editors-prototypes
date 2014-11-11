8ffa4c280aed2ec44b48c80e97205c2c57819051
Cindy Qi Li <cli@ocad.ca>
2014-11-11 11:32:41 -0500

    GPII-1014: Added the unit test for the cursor size enactor.

---
+++ tests/enactors/js/cursorSizeTests.js
@@ -0,0 +1,30 @@
+/*!
+Cloud4all Preferences Management Tools
+
+Copyright 2014 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/GPII/prefsEditors/LICENSE.txt
+*/
+
+(function () {
+    "use strict";
+
+    fluid.registerNamespace("gpii.tests");
+
+    jqUnit.test("Test the enactor that applies cursor size", function () {
+        jqUnit.expect(2);
+
+        var baseFontSize = 16;
+        var newValue = 300;
+        var that = gpii.enactor.cursorSize(".gpiic-cursorSize");
+
+        jqUnit.assertEquals("The initial font size is set", baseFontSize * 2 + "px", that.locate("cursorDiv").css("font-size"));
+        that.applier.change("value", newValue);
+        jqUnit.assertEquals("The new font size is applied based on the new model value " + newValue, baseFontSize * newValue / 100 + "px", that.locate("cursorDiv").css("font-size"));
+    });
+
+})();
