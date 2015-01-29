d9f6fd124fa2582ea9d9207961f7a113073d02d7
dimokas <dimokas@certh.gr>
2015-01-29 16:34:33 +0200

    GPII-1091: Running PMT without using Apache Web Server. [Step 1]: Finding available port. [Step 2]: Running http server with the use of kettle module. [Step 3]: Opening PMT url using default browser.

--- start.js
+++ start.js
@@ -11,10 +11,84 @@ https://github.com/GPII/prefsEditors/LICENSE.txt
 // Declare dependencies
 /* global require, __dirname */
 
-var util = require("util"),
-    connect = require("kettle/node_modules/express/node_modules/connect"),
-    port = 8888;
+var net = require('net'); 
+var async = require('async');
+var Socket = net.Socket; 
+var kettlePath = process.argv[2];
+var host = process.argv[3];
+var startPort = process.argv[4];
+var endPort = process.argv[5];
 
-connect.createServer(connect["static"](__dirname)).listen(port);
-util.puts("Preferences Management Tool server running...");
-util.puts("Visit http://localhost:" + port + "/demos/prefsEditor/index.html");
+var endingCondition = function(error, port) {
+    if (error) 
+        console.log(error);
+    else
+        startServer(port);
+}
+
+findPort(endingCondition);
+
+function startServer(port) {
+    var connect = require(kettlePath);
+    var browser = require('openurl'); // You should install openurl module. The command is: npm install openurl
+
+    connect.createServer(connect["static"](__dirname)).listen(port);
+    console.log("Preferences Management Tool server running...");
+    console.log("Visit http://localhost:" + port + "/demos/prefsEditor/index.html");
+    browser.open("http://localhost:" + port + "/demos/prefsEditor/index.html");
+}
+
+function findPort (callback) {
+    var foundPort = false;
+    port = startPort;
+
+    async.whilst(
+            function() {
+                return !foundPort;
+            }, 
+            function(callback) {
+                checkPort(port, function(error, status) {
+                    if (status === 'closed') {
+                        foundPort = true;
+                        callback(error);
+                    }
+                    else {
+                        port++;
+                        callback(null);
+                    }
+                });
+            }, 
+            function(error) {
+                if (error)
+                    callback(error, port);
+                else if (foundPort)
+                    callback(null, port);
+                else
+                    callback(null, false);
+            }
+    );
+}
+
+function checkPort (port, callback) {
+    var timeout = 1000;
+    var s = new Socket(), status = null, error = null;
+
+    s.on('connect', function() {
+        status = 'open';
+        s.destroy();
+    });
+    s.setTimeout(timeout)
+    s.on('timeout', function() {
+        status = 'closed';
+        error = new Error('Timeout for port ' + port);
+        s.destroy();
+    });
+    s.on('error', function(ex) {
+        error = ex;
+        status = 'closed';
+    });
+    s.on('close', function(ex) {
+        callback(null, status);
+    });
+    s.connect(port, host);
+}
